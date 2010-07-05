7d314ab7a9de7da69e99b7835e90e086c4fab2eb
Harper Reed <harper@nata2.org>
2010-07-05 15:02:42 -0500

    almost there. archives done, comments working, need tags/categories and then i will be done

---
+++ _extensions/archive_iterator.rb
@@ -0,0 +1,19 @@
+module Jekyll
+  AOP.around(Site, :site_payload) do |site_instance, args, proceed, abort|
+    monthly_archives = []
+
+    site_instance.collated.each do |year, hash|
+      hash.each do |month, days|
+        monthly_archives << {
+          'name'  => "#{Date::MONTHNAMES[month]} #{year}",
+          'url'   => "\/#{year}\/#{"%02d" % month}",
+          'posts' => days.values.flatten
+        }
+      end
+    end
+
+    result = proceed.call
+    result['site']['monthly_archives'] = monthly_archives
+    result
+  end
+end
