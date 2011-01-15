2a45554dac337bf04050bb71fd45a2a1fa2cdeb7
Harper Reed <harper@nata2.org>
2011-01-14 23:23:12 -0600

    added list of media to invalidate script

--- _tools/invalidate_cf_cdn.py
+++ _tools/invalidate_cf_cdn.py
@@ -3,7 +3,32 @@ from boto.cloudfront import CloudFrontConnection
 
 AWS_CF_DISTRIBUTION_ID = 'E3ZTU1ROX12QW'
 
-files = ['/css/style.css', ]
+files = ['/css/style.css',
+    '/favicon.ico',
+    '/images/harper_6.jpg',
+    '/images/glider.png',
+    '/js/scripts.js',
+    '/css/style.css',
+    '/images/DateHeaderIco.png',
+    '/images/PostComLeft.png',
+    '/images/PostComRight.png',
+    '/images/PostDetailsTagsIco.png',
+    '/images/RelatedPostsHeading.png',
+    '/images/RelatedPostsLi.png',
+    '/images/CommentsListHeading.png',
+    '/images/CommentsFormHeading.png',
+    '/images/SubmitComment.png',
+    '/images/SubmitComment.png',
+    '/images/SubmitComment.png',
+    '/images/CategIco.png',
+    '/images/tr.png',
+    '/images/tl.png',
+    '/images/br.png',
+    '/images/bl.png',
+    '/images/SRLBkg',
+    '/images/SRRBkg',
+    '/images/close2.png',]
+
 
 conn = CloudFrontConnection()
 print conn.create_invalidation_request(AWS_CF_DISTRIBUTION_ID, files)
